e10179050a46f90b31e0f0486a1165990359d94c
Elvin <elveng@gmail.com>
2019-02-20 18:35:30 -0500

    inital files

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+//JavaScript for Word Game on Dog Breeds
+
+/* Psuedocode 
+
+function function_name(argument) {
+    // body...
+}
+
+*/
+
+/*
+fetch('https://ekeoid.github.io/test.md')
+  .then(response => response.text())
+  .then(text => console.log(text))
+
+*/
+
+var dogs = JSON.parse(dogList);
+
+console.log(dogs[0].breed);
+console.log(dogs[0].image);
+console.log(dogs[1].breed);
+console.log(dogs[1].image);
+console.log(dogs[2].breed);
+console.log(dogs[2].image);
+console.log(dogs[3].breed);
+console.log(dogs[3].image);
